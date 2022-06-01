ec72ce8129a6bc0fadbe5d92c25358817fc2dfe3
elthedev <elthedev23@gamil.com>
2022-05-31 22:07:28 -0400

    first commit

---
+++ src/components/Button/Button.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+interface IButtonProps {
+  text: string;
+}
+
+const Button = (props: IButtonProps) =>  {
+  return <button>{props.text}</button>;
+}
+
+export default Button;
